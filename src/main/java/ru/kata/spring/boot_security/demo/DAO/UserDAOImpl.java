51246211e3da31a22a62ea253366ef62b236d617
Seawitcher <seawitcher83@yandex.ru>
2022-12-06 19:22:21 +0300

    Version 2.4 (Finish)

--- src/main/java/ru/kata/spring/boot_security/demo/DAO/UserDAOImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/DAO/UserDAOImpl.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.DAO;
 
 
+import org.hibernate.Criteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import ru.kata.spring.boot_security.demo.model.User;
 import javax.persistence.EntityManager;
 
 
-import javax.persistence.TypedQuery;
+
 import java.util.List;
 
 @Repository
@@ -32,7 +33,8 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public List<User> getList() {
-        return entityManager.createQuery("select u from User u", User.class).getResultList();
+        return entityManager.createQuery("select distinct u from User u left join fetch u.roles ", User.class)
+                .getResultList();
     }
 
     @Override
@@ -44,32 +46,12 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public void deleteUser(Long id) {
-//        User user = entityManager.find(User.class, id);
-//
-//// get all books that this author wrote alone
-//        Query q = entityManager.createNativeQuery("SELECT ur.role_id FROM user_roles ur JOIN role r ON ur.role_id = r.id JOIN user_roles ur2 ON r.id = ur2.role_id WHERE ur2.role_id = ? GROUP BY ur.role_id HAVING count(ur.user_id) = 1");
-//        q.setParameter(1, user.getId());
-//        List<Long> role_ids = (List<Long>)q.getResultList();
-//
-//// remove all associations for this author
-//        q = entityManager.createNativeQuery("DELETE FROM user_roles ur WHERE ur.user_id = ?");
-//        q.setParameter(1, user.getId());
-//        q.executeUpdate();
-//
-//// remove all books that this author wrote alone
-//        q = entityManager.createNativeQuery("DELETE FROM Role r WHERE r.id IN (:ids)");
-//        q.setParameter("ids", role_ids);
-//        q.executeUpdate();
-//
-//// remove author
-//        q = entityManager.createNativeQuery("DELETE FROM Users u WHERE u.id =:id");
-//        q.setParameter("id", user.getId());
-//        q.executeUpdate();
 
 
         entityManager.createQuery(
                 "DELETE User WHERE id = :id").setParameter("id", id).executeUpdate();
-//        entityManager.remove(getUser(id));
+
+
     }
 
     @Override
@@ -79,12 +61,10 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public UserDetails getUser(String username) {
-        TypedQuery<User> query = entityManager.createQuery(
-                "SELECT u FROM User u WHERE u.name = :username", User.class);
-
-        return query.setParameter("username", username)
-               .getSingleResult();
 
 
+        return entityManager.createQuery(
+                        "SELECT u FROM User u WHERE u.name = :username", User.class).setParameter("username", username)
+                .getSingleResult();
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.GrantedAuthority;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
